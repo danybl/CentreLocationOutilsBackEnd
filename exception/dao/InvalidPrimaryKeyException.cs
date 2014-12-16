71a8813b0199b59d13f4703205a1c75062644a54
CedricSoum <Cedric.Soumpholphakdy.1@collegeahuntsic.qc.ca>
2014-12-15 22:47:38 -0500

    les exceptions DAO

--- exception/dao/InvalidPrimaryKeyException.cs
+++ exception/dao/InvalidPrimaryKeyException.cs
@@ -7,18 +7,17 @@ namespace CentreLocationOutils.exception.dao
 {
     public class InvalidPrimaryKeyException : Exception
     {
-        /**
-       * Constructs a new exception with null as its detail message. The cause is not initialized, and may subsequently be initialized by a call
-       * to {@link java.lang.Throwable#initCause(java.lang.Throwable) Throwable.initCause(Throwable)}.
-       */
+       ///<summary>
+       /// Constructs a new exception with null as its detail message. The cause is not initialized, and may subsequently be initialized by a call
+       /// to {@link java.lang.Throwable#initCause(java.lang.Throwable) Throwable.initCause(Throwable)}.
+        ///</summary>
         public InvalidPrimaryKeyException() : base() { }
-        /**
-       * Constructs a new exception with the specified detail message. The cause is not initialized, and may subsequently be initialized by a call
-       * to {@link java.lang.Throwable#initCause(java.lang.Throwable) Throwable.initCause(Throwable)}.
-       * 
-       * @param message The detail message. The detail message is saved for later retrieval by the
-       *        {@link java.lang.Throwable#getMessage() Throwable.getMessage()} method
-       */
+       ///<summary>
+       /// Constructs a new exception with the specified detail message. The cause is not initialized, and may subsequently be initialized by a call
+       /// to {@link java.lang.Throwable#initCause(java.lang.Throwable) Throwable.initCause(Throwable)}. 
+       /// @param message The detail message. The detail message is saved for later retrieval by the
+       ///        {@link java.lang.Throwable#getMessage() Throwable.getMessage()} method
+        ///</summary>
         public InvalidPrimaryKeyException(string message) : base(message) { }
     }
 }
