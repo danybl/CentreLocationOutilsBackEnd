e05616334fb6051feb81c2a9ef5629a0e5295e00
CedricSoum <Cedric.Soumpholphakdy.1@collegeahuntsic.qc.ca>
2014-12-15 20:37:11 -0500

    EmployeFacade commentaires

--- facade/interfaces/IEmployeFacade.cs
+++ facade/interfaces/IEmployeFacade.cs
@@ -8,24 +8,30 @@ using System.Text;
 namespace CentreLocationOutils.facade.interfaces
 {
     /// <summary>
-    /// Interface de façade pour manipuler les employes dans la base de données.
+    /// Interface de façade pour manipuler les employés dans la base de données.
     /// </summary>
     public interface IEmployeFacade : IFacade
     {
         /// <summary>
-        /// Inscrire un employe
+        /// Inscrire un employé
         /// </summary>
         /// <param name="connection">La connexion à utiliser</param>
-        /// <param name="employeDTO">L'employe à inscrire</param>
+        /// <param name="employeDTO">L'employé à inscrire</param>
         void inscrireEmploye(Connection connection, EmployeDTO employeDTO);
 
         /// <summary>
-        /// Desinscrire un employe
+        /// Desinscrire un employé
         /// </summary>
         /// <param name="connection">La connexion à utiliser</param>
-        /// <param name="employeDTO">L'employe à desinscrire</param>
+        /// <param name="employeDTO">L'employé à desinscrire</param>
         void desinscrireEmploye(Connection connection, EmployeDTO employeDTO);
 
+        /// <summary>
+        /// Lit un employé à partir de la base de données.
+        /// </summary>
+        /// <param name="connection">La connection à utiliser</param>
+        /// <param name="idEmploye">L'id de L'employé</param>
+        /// <returns></returns>
         EmployeDTO getEmploye(Connection connection, string idEmploye);
     }
 }
