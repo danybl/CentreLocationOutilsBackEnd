af81e7099c64484c36b4cbc49062eed8ae95db42
CedricSoum <Cedric.Soumpholphakdy.1@collegeahuntsic.qc.ca>
2014-12-15 23:14:22 -0500

    les exceptions de util

--- exception/util/CentreCreateurException.cs
+++ exception/util/CentreCreateurException.cs
@@ -7,18 +7,17 @@ namespace CentreLocationOutils.exception.util
 {
     public class CentreCreateurException : Exception
     {
-        /**
-      * Constructs a new exception with null as its detail message. The cause is not initialized, and may subsequently be initialized by a call
-      * to {@link java.lang.Throwable#initCause(java.lang.Throwable) Throwable.initCause(Throwable)}.
-      */
+      ///<summary>
+      /// Constructs a new exception with null as its detail message. The cause is not initialized, and may subsequently be initialized by a call
+      /// to {@link java.lang.Throwable#initCause(java.lang.Throwable) Throwable.initCause(Throwable)}.
+      ///</summary>
         public CentreCreateurException() : base() { }
-        /**
-       * Constructs a new exception with the specified detail message. The cause is not initialized, and may subsequently be initialized by a call
-       * to {@link java.lang.Throwable#initCause(java.lang.Throwable) Throwable.initCause(Throwable)}.
-       * 
-       * @param message The detail message. The detail message is saved for later retrieval by the
-       *        {@link java.lang.Throwable#getMessage() Throwable.getMessage()} method
-       */
+      ///<summary>
+      /// Constructs a new exception with the specified detail message. The cause is not initialized, and may subsequently be initialized by a call
+      /// to {@link java.lang.Throwable#initCause(java.lang.Throwable) Throwable.initCause(Throwable)}. 
+      /// @param message The detail message. The detail message is saved for later retrieval by the
+      ///        {@link java.lang.Throwable#getMessage() Throwable.getMessage()} method
+      ///</summary>
         public CentreCreateurException(string message) : base(message) { }
 
         public CentreCreateurException(string message, Exception cause) : base(message, cause) { }
